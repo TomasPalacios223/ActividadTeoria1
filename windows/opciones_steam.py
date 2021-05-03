ff111bb94921a8ec9fa38224aac5913ddd1383c3
tomas palacios <tomypalacios.25@gmail.com>
2021-05-03 13:28:32 -0300

    actividad final corregida

--- windows/opciones_steam.py
+++ windows/opciones_steam.py
@@ -5,7 +5,7 @@ def build():
     Construye la ventana del menú de opciones de Steam
     """
     layout = [[sg.Button('Buscar segun juegos gratis o pagos', size=(50, 2), key="-type-")],
-             [sg.Button('Imprimir los 10 juegos  pagos mas jugados', size=(50, 2), key="-mas_jugados-")],
+             [sg.Button('Imprimir los 4 juegos  pagos mas jugados', size=(50, 2), key="-mas_jugados-")],
              [sg.Button('Salir', size=(50, 2), key="-exit-")]]
 
     board = sg.Window('Menu de opciones de STEAM').Layout(layout)
